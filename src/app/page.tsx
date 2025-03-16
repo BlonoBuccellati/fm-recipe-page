377e29083ca35008f04b55ab5dd7a7856d2d80a6
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-03-16 20:20:39 +0900

    style(design system): フォントを追加
    使用するフォントを追加。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <div>
+      <div>test</div>
+      <div className='font-young text-3xl'>young!</div>
+    </div>
+  );
 }
