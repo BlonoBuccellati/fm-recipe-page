db59e3c3c3c69add31b6cd7c7a6b7f6629a56ab3
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-03-21 16:17:02 +0900

    chore(markup):レイアウト用のHTLM構造を仮で追加。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
 export default function Home() {
   return (
-    <div>
-      <div>test</div>
-      <div className='font-young text-3xl'>young!</div>
+    <div className=''>
+      {/* Image */}
+      {/* Content */}
+      <div>
+        {/* Header */}
+        <h1>Simple Omelette Recipe</h1>
+        {/* paragraph */}
+        <p></p>
+        {/* preparation time */}
+        <div>
+          <h3>Preparation time</h3>
+          {/* リスト */}
+        </div>
+        {/* Ingredients */}
+        <div>
+          <h2>Ingredients</h2>
+          <ul>
+            <li>リスト</li>
+          </ul>
+        </div>
+        {/* Instructions */}
+        <div>
+          <h2>Instructions</h2>
+          {/* リスト */}
+        </div>
+        <div>
+          <h2>Nutrition</h2>
+          {/* paragraph */}
+          {/* table */}
+        </div>
+      </div>
     </div>
   );
 }
