0601bda687fa457cf07e58d46fffb6b1f4ae2043
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-03-21 21:04:55 +0900

    style(レスポンシブ対応): レスポンシブデザインに対応し、各画面で最適化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,29 @@ import Title from '@/_components/title';
 import Image from 'next/image';
 export default function Home() {
   return (
-    // ここでカードになってる
-    <div className='flex flex-col items-center justify-center'>
-      {/* Image */}
-      <div className='relative h-[170px] w-full'>
-        <Image src='/assets/images/image-omelette.jpeg' alt='Omelette' fill />
-      </div>
-      {/* Content */}
-      <div className='p-8 flex flex-col gap-5'>
-        {/* ここで一個は盲点だった。 */}
-        <Title />
-        <PreparationTime />
-        <Ingredients />
-        <Instructions />
-        <Nutrition />
+    <div className='md:bg-Stone-100 flex justify-center'>
+      {/* ここでカードになってる */}
+      <div className='flex flex-col items-center justify-center md:w-[616px] bg-white md:rounded-2xl md:p-10 md:gap-10 md:my-32'>
+        {/* Image */}
+        <div className='relative h-[171px] md:h-[300px] w-full'>
+          <Image
+            src='/assets/images/image-omelette.jpeg'
+            alt='Omelette'
+            fill
+            className='md:rounded-2xl '
+          />
+        </div>
+        {/* Content */}
+        <div className='px-8 py-10 flex flex-col gap-8 md:p-0'>
+          {/* ここで一個は盲点だった。 */}
+          <Title />
+          <PreparationTime />
+          <Ingredients />
+          <hr className='border-t border-Stone-150' />
+          <Instructions />
+          <hr className='border-t border-Stone-150' />
+          <Nutrition />
+        </div>
       </div>
     </div>
   );
