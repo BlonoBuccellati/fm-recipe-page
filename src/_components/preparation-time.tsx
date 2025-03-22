111340bd9f83fdacdfb748126c014ba26b2925da
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-03-22 22:11:48 +0900

    fix(preparationTime): h3タグをh2タグに修正
    アクセシビリティの観点から、h1->h2のため。

--- src/_components/preparation-time.tsx
+++ src/_components/preparation-time.tsx
@@ -3,9 +3,9 @@ import UlListItem from '@/_components/ul-list-item';
 const PreparationTime = () => {
   return (
     <div className='bg-Rose-50 rounded-xl flex flex-col gap-4 p-6'>
-      <h3 className='text-Rose-800 text-xl font-semibold leading-none'>
+      <h2 className='text-Rose-800 text-xl font-semibold leading-none'>
         Preparation time
-      </h3>
+      </h2>
       <ul className='flex flex-col gap-2'>
         <UlListItem>
           <b>Total</b>: Approximately 10 minutes
